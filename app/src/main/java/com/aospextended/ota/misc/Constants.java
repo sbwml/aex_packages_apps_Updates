PixelExperience/packages_apps_Updates

Add multi version support
* Also add a separated prop to device name

app/src/main/java/org/pixelexperience/ota/misc/Constants.java
@@ -33,8 +33,8 @@ public class Constants {
     // Build vars
     public static final String CURRENT_DEVICE_NAME = "ro.product.device";
     public static final String CURRENT_VERSION = "org.pixelexperience.version";
-    public static final String CURRENT_BUILD_TYPE = "org.pixelexperience.build_type";
-    public static final String OTA_URL = "https://download.pixelexperience.org/ota/%s";
+    public static final String OTA_URL = "https://download.pixelexperience.org/ota/%s/%s";
+    public static final String OTA_VERSION_CODE = "org.pixelexperience.ota.version_code";
     // Expressions
     public static final String FILENAME_DATE_FORMAT = "yyyyMMdd-Hm";
     public static final String PATTERN_FILENAME_DATE_FORMAT = "(\\d{4}\\d{2}\\d{2}-\\d{2}\\d{2})";

sbwml/aex_packages_apps_Updates

Change-Id: I0f9d2b33533fe638c460963ca55ae62b2b9de88f
Signed-off-by: Shubham Singh <coolsks94@gmail.com>

app/src/main/java/com/aospextended/ota/misc/Constants.java
@@ -33,7 +33,8 @@ public class Constants {
     public static final String CURRENT_DEVICE_NAME = "ro.aex.device";
     public static final String CURRENT_VERSION = "ro.modversion";
     public static final String CURRENT_BUILD_TYPE = "ro.extended.releasetype";
-    public static final String OTA_URL = "https://downloads.aospextended.com/ota/%s";
+    public static final String OTA_URL = "https://downloads.aospextended.com/ota/%s/%s";
+    public static final String OTA_VERSION_CODE = "ro.extended.ota.version_code";
     // Expressions
     public static final String FILENAME_DATE_FORMAT = "yyyyMMdd-Hm";
     public static final String PATTERN_FILENAME_DATE_FORMAT = "(\\d{4}\\d{2}\\d{2}-\\d{2}\\d{2})";